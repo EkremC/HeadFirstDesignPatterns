7fc5187c1cdad51b41a48f190b0c1a7b732196e4
EkremC <ekremccandemir@gmail.com>
2020-05-18 01:54:05 +0300

    Ch03_TheDecoratorPattern

---
+++ src/main/java/Chapter03_TheDecoratorPattern/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package Chapter03_TheDecoratorPattern;
+
+public abstract class CondimentDecorator extends Beverage {
+    public abstract String getDescription();
+}
---
+++ src/main/java/Chapter03_TheDecoratorPattern/Decaf.java
@@ -0,0 +1,13 @@
+package Chapter03_TheDecoratorPattern;
+
+public class Decaf extends Beverage {
+
+    public Decaf() {
+        description = "Decaf";
+    }
+
+    @Override
+    public double cost() {
+        return 1.05;
+    }
+}
