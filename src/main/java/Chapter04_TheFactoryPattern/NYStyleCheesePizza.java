ce7d4384d36f8caadda1562a840556f6bd0f5990
EkremC <ekremccandemir@gmail.com>
2020-05-24 07:37:33 +0300

    Ch04_TheFactoryPattern PizzaFactory

---
+++ src/main/java/Chapter04_TheFactoryPattern/NYStyleCheesePizza.java
@@ -0,0 +1,12 @@
+package Chapter04_TheFactoryPattern;
+
+public class NYStyleCheesePizza extends Pizza {
+
+	public NYStyleCheesePizza() {
+		name = "NY Style Sauce and Cheese Pizza";
+		dough = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+
+		toppings.add("Grated Reggiano Cheese");
+	}
+}
