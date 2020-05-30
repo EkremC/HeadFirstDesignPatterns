76ee694d6f3352469f80c6a32e32953cd625961c
EkremC <ekremccandemir@gmail.com>
2020-05-30 22:15:47 +0300

    Ch04_TheCommandPattern simpleremote

---
+++ src/main/java/Chapter06_TheCommandPattern/simpleremote/Command.java
@@ -0,0 +1,5 @@
+package Chapter06_TheCommandPattern.simpleremote;
+
+public interface Command {
+    public void execute();
+}
---
+++ src/main/java/Chapter06_TheCommandPattern/simpleremote/SimpleRemoteControl.java
@@ -0,0 +1,16 @@
+package Chapter06_TheCommandPattern.simpleremote;
+
+public class SimpleRemoteControl {
+    Command slot;
+
+    public SimpleRemoteControl() {
+    }
+
+    public void setCommand(Command command) {
+        this.slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
