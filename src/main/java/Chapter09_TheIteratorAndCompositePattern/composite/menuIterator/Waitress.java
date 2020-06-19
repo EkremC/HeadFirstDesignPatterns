266d350d981b50bdf0457359213a507088873b8d
EkremC <ekremccandemir@gmail.com>
2020-06-19 07:21:43 +0300

    Ch09_TheIteratorAndCompositePattern composite iterator

---
+++ src/main/java/Chapter09_TheIteratorAndCompositePattern/composite/menuIterator/Waitress.java
@@ -0,0 +1,30 @@
+package Chapter09_TheIteratorAndCompositePattern.composite.menuIterator;
+
+import java.util.Iterator;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printVegetarianMenu() {
+        Iterator<MenuComponent> iterator = allMenus.createIterator();
+
+        System.out.println("\nVEGETARIAN MENU\n------");
+        while (iterator.hasNext()) {
+            final MenuComponent menuComponent = iterator.next();
+            try {
+                if (menuComponent.isVegetarian()) {
+                    menuComponent.print();
+                }
+            } catch (UnsupportedOperationException e) {
+            }
+        }
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
